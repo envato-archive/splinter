040232c06552d401b811abd92ecaeea035353008
Joshua Priddle <jpriddle@me.com>
2012-09-10 14:22:48 -0400

    Remove FormCompleter "stack" from `complete_form`
    Instead of maintaining a stack of Capybara actions to run, just fire them as
    soon as we yield the FormCompleter. This allows arbitrary code to be run in
    the complete_form block, eg:
    
        complete_form :blah do
          f.text_field :name, "Test"
          wait_until { page.find "#blah" }
        end

--- lib/splinter/capybara/form_completer.rb
+++ lib/splinter/capybara/form_completer.rb
@@ -3,42 +3,38 @@ module Splinter
     # The FormCompleter is a simple stack that keeps track of Capybara actions
     # to run.
     class FormCompleter
-      def initialize(base)
-        @base  = base
-        @stack = []
+      def initialize(base, page)
+        @base = base
+        @page = page
       end
 
       def text_field(id, value)
-        @stack << [:fill_in, "#{@base}_#{id}", { :with => value.to_s }]
+        @page.send :fill_in, "#{@base}_#{id}", { :with => value.to_s }
       end
       alias text_area text_field
 
       def checkbox(id, value = nil)
-        @stack << [value ? :check : :uncheck, "#{@base}_#{id}"]
+        @page.send value ? :check : :uncheck, "#{@base}_#{id}"
       end
 
       def radio(id, value)
-        @stack << [:choose, "#{@base}_#{id}_#{value}"]
+        @page.send :choose, "#{@base}_#{id}_#{value}"
       end
 
       def select(id, value)
-        @stack << [:find_and_select_option, "#{@base}_#{id}", value]
+        @page.send :find_and_select_option, "#{@base}_#{id}", value
       end
 
       def time(id, value)
-        @stack << [:select_time, value, { :id_prefix => "#{@base}_#{id}" }]
+        @page.send :select_time, value, { :id_prefix => "#{@base}_#{id}" }
       end
 
       def date(id, value)
-        @stack << [:select_date, value, { :id_prefix => "#{@base}_#{id}" }]
+        @page.send :select_date, value, { :id_prefix => "#{@base}_#{id}" }
       end
 
       def datetime(id, value)
-        @stack << [:select_datetime, value, { :id_prefix => "#{@base}_#{id}" }]
-      end
-
-      def each_input(&block)
-        @stack.each(&block)
+        @page.send :select_datetime, value, { :id_prefix => "#{@base}_#{id}" }
       end
     end
   end
